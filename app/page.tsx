7b7c87559f318b3cb04a33dc1dd0389c776ac416
JusAeng <justo_play@hotmail.com>
2023-12-30 14:05:37 +0700

    normal

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,8 @@
 import AnimeCard from "./components/AnimeCard";
+import { AnimeType } from "./types/Anime";
+
+import testData from "./temp/anime.json";
 
 export default function Home() {
-  return (
-    <main className="bg-[#f0f6ff] h-[100vh]">
-      <AnimeCard
-        image={
-          "https://static.wikia.nocookie.net/rehero/images/4/44/Megumin.jpg/revision/latest/scale-to-width-down/1200?cb=20180330001646"
-        }
-        name={"One Piece"}
-        author={"Endo"}
-        lastChaper={17}
-      />
-      <AnimeCard
-        image={
-          "https://static.wikia.nocookie.net/rehero/images/4/44/Megumin.jpg/revision/latest/scale-to-width-down/1200?cb=20180330001646"
-        }
-        name={"One Piece"}
-        author={"Endo"}
-        lastChaper={17}
-      />
-    </main>
-  );
+  return <main className="bg-[#f0f6ff] h-[100vh]"></main>;
 }
