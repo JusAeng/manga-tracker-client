1b552330febf631635359ef719790a820a63ab63
JusAeng <justo_play@hotmail.com>
2023-12-30 14:06:16 +0700

    add prop

--- app/components/AnimeCard/index.tsx
+++ app/components/AnimeCard/index.tsx
@@ -1,23 +1,34 @@
 import Image from "next/image";
-import "./index.scss";
 
-const AnimeCard = () => {
+interface Iprop {
+  image: string;
+  name: string;
+  author: string;
+  lastEpisode: number;
+}
+
+const AnimeCard: React.FC<Iprop> = ({ image, name, author, lastEpisode }) => {
   return (
-    <main className="flex bg-[#f0f6ff] w-[335px] h-[96px] rounded-xl">
-      <section className="m-[10px]">
-        <div className="anime-card-image"></div>
-        {/* <Image
-          src="https://static.wikia.nocookie.net/rehero/images/4/44/Megumin.jpg/revision/latest/scale-to-width-down/1200?cb=20180330001646"
-          alt="Picture of the author"
-          width={68}
-          height={10}
-          className="rounded-xl"
-        /> */}
+    <main className="flex bg-[#ffffff] w-[335px] h-[96px] rounded-xl">
+      <section className="m-[10px] grid place-items-center w-[25%]">
+        <div className="relative w-[68px] h-[68px]">
+          <Image
+            src={image}
+            alt="Picture of the author"
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+            className="rounded-xl"
+          />
+        </div>
       </section>
-      <section className="m-[10px]">
-        <div>One Piece</div>
-        <div>3,455,192</div>
-        <div className="bg-[#fa9daa] rounded-lg px-[3px]">Chapter 17</div>
+      <section className="m-[10px] w-[60%]">
+        <h3>{name}</h3>
+        <p className="text-sm text-[#9e9e9e]">{author}</p>
+        <span className="bg-[#fab6b1] rounded-lg px-[3px] text-[11px]">
+          Chapter {lastEpisode}
+        </span>
       </section>
     </main>
   );
