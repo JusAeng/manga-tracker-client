37a5a0835fd5a6303e8c851c8857c9a5aaaaa839
JusAeng <justo_play@hotmail.com>
2024-01-02 03:23:03 +0700

    display anime card

--- app/shelf/page.tsx
+++ app/shelf/page.tsx
@@ -1,5 +1,24 @@
+import AnimeCard from "../components/AnimeCard";
+import testData from "../temp/anime.json";
+
 const ShelfPage = () => {
-  return <div>This is ShelfPage</div>;
+  const myAnimes = testData;
+
+  return (
+    <main className="flex flex-col items-center gap-[10px] bg-[#a0f6ff]">
+      {myAnimes.map((data) => {
+        return (
+          <AnimeCard
+            key={data.name}
+            image={data.image}
+            name={data.name}
+            author={data.author}
+            lastEpisode={data.lastEpisode}
+          />
+        );
+      })}
+    </main>
+  );
 };
 
 export default ShelfPage;
