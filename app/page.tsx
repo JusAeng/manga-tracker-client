5d6b1dc25efa3023dd7d346e2b287adfefd40d60
JusAeng <justo_play@hotmail.com>
2024-01-02 03:21:42 +0700

    temp

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-import AnimeCard from "./components/AnimeCard";
-import { AnimeType } from "./types/Anime";
-
-import testData from "./temp/anime.json";
-
 export default function Home() {
   return <main className="bg-[#f0f6ff] h-[100vh]"></main>;
 }
