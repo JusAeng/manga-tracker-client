5a4b04dc60023dde8022832580cf0eeaad95a3df
JusAeng <justo_play@hotmail.com>
2024-01-02 03:22:01 +0700

    search

---
+++ app/search/SearchBar.tsx
@@ -0,0 +1,9 @@
+const SearchBar = () => {
+  return (
+    <main>
+      <input type="text" />
+    </main>
+  );
+};
+
+export default SearchBar;
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,12 @@
+import SearchBar from "./SearchBar";
+
 const SearchPage = () => {
-  return <div>This is search</div>;
+  return (
+    <div>
+      <p>This is Search page</p>
+      <SearchBar />
+    </div>
+  );
 };
 
 export default SearchPage;
