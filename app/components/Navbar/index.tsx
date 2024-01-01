6d814ec4351348273e4eb2276dc6fd34b456bb9c
JusAeng <justo_play@hotmail.com>
2024-01-02 03:22:44 +0700

    Change navbar

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import { SpeedDial, SpeedDialAction, SpeedDialIcon } from "@mui/material";
-
 import { RiHomeFill, RiSearchFill } from "react-icons/ri";
 import { ImBooks } from "react-icons/im";
 import { FaUserAlt } from "react-icons/fa";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface ActionType {
   icon: JSX.Element;
@@ -13,43 +12,46 @@ interface ActionType {
   link: string;
 }
 
+interface MenuIconProp {
+  action: ActionType;
+  color: string;
+}
+
 const actions: ActionType[] = [
-  { icon: <RiHomeFill size={16} />, name: "Home", link: "/" },
-  { icon: <RiSearchFill size={16} />, name: "Search", link: "/search" },
-  { icon: <ImBooks size={16} />, name: "Shelf", link: "/shelf" },
-  { icon: <FaUserAlt size={16} />, name: "Profile", link: "/profile" },
+  { icon: <RiHomeFill size={20} />, name: "Home", link: "/" },
+  { icon: <RiSearchFill size={20} />, name: "Search", link: "/search" },
+  { icon: <ImBooks size={20} />, name: "Shelf", link: "/shelf" },
+  { icon: <FaUserAlt size={20} />, name: "Profile", link: "/profile" },
 ];
+
+const MenuIcon: React.FC<MenuIconProp> = ({ action, color }) => (
+  <div style={{ color }}>{action.icon}</div>
+);
 
 const Navbar = () => {
   const router = useRouter();
+  const [menu, setMenu] = useState("Home");
 
   const handleSpeedDialAction = (action: ActionType) => {
+    setMenu(action.name);
     router.push(action.link);
     console.log(action.link);
   };
 
   return (
-    <main className="flex">
-      <SpeedDial
-        ariaLabel="SpeedDial example"
-        icon={<SpeedDialIcon sx={{ color: "#ffffff" }} />}
-        FabProps={{
-          style: {
-            backgroundColor: "#2c7ee8",
-            backgroundSize: "cover",
-          },
-        }}
-        direction="right"
-      >
-        {actions.map((action) => (
-          <SpeedDialAction
-            key={action.name}
-            icon={action.icon}
-            tooltipTitle={action.name}
-            onClick={() => handleSpeedDialAction(action)}
+    <main className="flex border justify-around">
+      {actions.map((action) => (
+        <div
+          key={action.name}
+          className="border grid place-items-center cursor-pointer"
+          onClick={() => handleSpeedDialAction(action)}
+        >
+          <MenuIcon
+            action={action}
+            color={menu === action.name ? "#0000AA" : "#000000"}
           />
-        ))}
-      </SpeedDial>
+        </div>
+      ))}
     </main>
   );
 };
