8a8a9d17a386c8be2121ba76c60da94834e51ade
JusAeng <justo_play@hotmail.com>
2024-01-07 01:34:01 +0700

    to check that provider cover layout is not dup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import SearchProvider from "./contexts/search/SearchProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className + " bg-[#ffffff]"}>
-        {children}
-        <Navbar />
+        <SearchProvider>
+          {children}
+          <Navbar />
+        </SearchProvider>
       </body>
     </html>
   );
