2ec7d0d2191c2a70079bef083b0b6fe86d992943
JusAeng <justo_play@hotmail.com>
2024-01-07 22:01:07 +0700

    refactor

---
+++ app/shelf/MangaContainer.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import AnimeCard from "../components/AnimeCard";
+import useSearch from "../hooks/UseSearch";
+import testData from "../temp/anime.json";
+
+const MangaContainer = () => {
+  const myAnimes = testData;
+  const { searchText } = useSearch();
+
+  let filtered = myAnimes?.filter(
+    (manga) =>
+      manga.name.toLowerCase().includes(searchText.toLowerCase()) ||
+      manga.author.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  return (
+    <div className="flex flex-col items-center gap-[10px] mt-[10px]">
+      {filtered.map((data) => {
+        return (
+          <AnimeCard
+            key={data.name}
+            image={data.image}
+            name={data.name}
+            author={data.author}
+            lastEpisode={data.lastEpisode}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default MangaContainer;
--- app/shelf/page.tsx
+++ app/shelf/page.tsx
@@ -1,26 +1,11 @@
-import AnimeCard from "../components/AnimeCard";
 import MenuBar from "../components/MenuBar";
-import testData from "../temp/anime.json";
+import MangaContainer from "./MangaContainer";
 
 const ShelfPage = () => {
-  const myAnimes = testData;
-
   return (
     <main>
       <MenuBar head={"My Manga"} sort={true} search={true} />
-      <div className="flex flex-col items-center gap-[10px] mt-[10px]">
-        {myAnimes.map((data) => {
-          return (
-            <AnimeCard
-              key={data.name}
-              image={data.image}
-              name={data.name}
-              author={data.author}
-              lastEpisode={data.lastEpisode}
-            />
-          );
-        })}
-      </div>
+      <MangaContainer />
     </main>
   );
 };
