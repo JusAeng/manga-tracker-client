d31c44ad8a5694fecb258a014bf423c553ba9e18
JusAeng <justo_play@hotmail.com>
2024-01-08 00:39:48 +0700

    create profile card

---
+++ app/profile/ProfileCard.tsx
@@ -0,0 +1,14 @@
+const ProfileCard = () => {
+  return (
+    <main className="shadow-card h-[200px] w-[370px] rounded-[20px] grid grid-cols-2 p-[10px]">
+      <section className="">1</section>
+      <section className="">
+        <p>zPlanC</p>
+        <p>All Subscribe:</p>
+        <p>All Manga:</p>
+      </section>
+    </main>
+  );
+};
+
+export default ProfileCard;
