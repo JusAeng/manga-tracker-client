03fbb9eb82559f1d39ed8d3dd6c64d36a57b43bf
JusAeng <justo_play@hotmail.com>
2024-01-08 00:59:35 +0700

    sticky menubar on top

--- app/components/MenuBar/index.tsx
+++ app/components/MenuBar/index.tsx
@@ -37,7 +37,7 @@ const MenuBar: React.FC<Iprop> = ({ head, rank, sort, search }) => {
   }, [setSearchText]);
 
   return (
-    <main className="bg-[#5c7df7] h-[40px] grid place-items-center">
+    <main className="bg-[#5c7df7] h-[40px] grid place-items-center w-[100%] sticky top-[0px]">
       {isSearch ? (
         <div className="flex justify-between px-[10px] w-[100%]">
           <div onClick={handleSearch}>back</div>
