d5ca453da30385e2fee0377f57bd8a525db604a9
JusAeng <justo_play@hotmail.com>
2024-01-08 21:33:31 +0700

    hide scroll bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className + " bg-[#ffffff]"}>
+      <body className={inter.className + " bg-[#ffffff] scrollbar-hide"}>
         <SearchProvider>
           {children}
           <Navbar />
