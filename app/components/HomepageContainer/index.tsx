ef0fdab542e0ed0785c24b55daecf42cb2eb74f8
JusAeng <justo_play@hotmail.com>
2024-01-08 21:34:27 +0700

    try to add component

--- app/components/HomepageContainer/index.tsx
+++ app/components/HomepageContainer/index.tsx
@@ -15,7 +15,27 @@ const HomeContainer = () => {
         name={myAnimes[0].name}
       />
       <p>Trending Manga</p>
-      <div className="flex gap-[10px]">
+      <div className="flex gap-[10px] overflow-x-auto whitespace-nowrap scrollbar-hide bg-[#e0e0e0] p-[10px]">
+        <TrendingCard
+          image={myAnimes[1].image}
+          name={myAnimes[0].name}
+          author={myAnimes[0].author}
+        />
+        <TrendingCard
+          image={myAnimes[0].image}
+          name={myAnimes[0].name}
+          author={myAnimes[0].author}
+        />
+        <TrendingCard
+          image={myAnimes[1].image}
+          name={myAnimes[0].name}
+          author={myAnimes[0].author}
+        />
+        <TrendingCard
+          image={myAnimes[0].image}
+          name={myAnimes[0].name}
+          author={myAnimes[0].author}
+        />
         <TrendingCard
           image={myAnimes[1].image}
           name={myAnimes[0].name}
@@ -27,38 +47,73 @@ const HomeContainer = () => {
           author={myAnimes[0].author}
         />
       </div>
+      <div>
+        <p className="mb-[10px]">Recommend</p>
+        <div className="flex gap-[10px] overflow-x-auto whitespace-nowrap scrollbar-hide bg-[#e0e0e0] p-[10px]">
+          <TrendingCard
+            image={myAnimes[1].image}
+            name={myAnimes[0].name}
+            author={myAnimes[0].author}
+          />
+          <TrendingCard
+            image={myAnimes[0].image}
+            name={myAnimes[0].name}
+            author={myAnimes[0].author}
+          />
+          <TrendingCard
+            image={myAnimes[1].image}
+            name={myAnimes[0].name}
+            author={myAnimes[0].author}
+          />
+          <TrendingCard
+            image={myAnimes[0].image}
+            name={myAnimes[0].name}
+            author={myAnimes[0].author}
+          />
+          <TrendingCard
+            image={myAnimes[1].image}
+            name={myAnimes[0].name}
+            author={myAnimes[0].author}
+          />
+          <TrendingCard
+            image={myAnimes[0].image}
+            name={myAnimes[0].name}
+            author={myAnimes[0].author}
+          />
+        </div>
+      </div>
       <div className="mb-[30px]">
-        <p>Recommend</p>
-        <div className="flex flex-col items-center">
-          <AnimeCard
-            image={myAnimes[4].image}
-            name={myAnimes[4].name}
-            author={myAnimes[4].author}
-            lastEpisode={12}
+        <p className="mb-[10px]">New</p>
+        <div className="flex gap-[10px] overflow-x-auto whitespace-nowrap scrollbar-hide bg-[#e0e0e0] p-[10px]">
+          <TrendingCard
+            image={myAnimes[1].image}
+            name={myAnimes[0].name}
+            author={myAnimes[0].author}
           />
-          <AnimeCard
-            image={myAnimes[4].image}
-            name={myAnimes[4].name}
-            author={myAnimes[4].author}
-            lastEpisode={12}
+          <TrendingCard
+            image={myAnimes[0].image}
+            name={myAnimes[0].name}
+            author={myAnimes[0].author}
           />
-          <AnimeCard
-            image={myAnimes[4].image}
-            name={myAnimes[4].name}
-            author={myAnimes[4].author}
-            lastEpisode={12}
+          <TrendingCard
+            image={myAnimes[1].image}
+            name={myAnimes[0].name}
+            author={myAnimes[0].author}
           />
-          <AnimeCard
-            image={myAnimes[4].image}
-            name={myAnimes[4].name}
-            author={myAnimes[4].author}
-            lastEpisode={12}
+          <TrendingCard
+            image={myAnimes[0].image}
+            name={myAnimes[0].name}
+            author={myAnimes[0].author}
           />
-          <AnimeCard
-            image={myAnimes[4].image}
-            name={myAnimes[4].name}
-            author={myAnimes[4].author}
-            lastEpisode={12}
+          <TrendingCard
+            image={myAnimes[1].image}
+            name={myAnimes[0].name}
+            author={myAnimes[0].author}
+          />
+          <TrendingCard
+            image={myAnimes[0].image}
+            name={myAnimes[0].name}
+            author={myAnimes[0].author}
           />
         </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import HomeContainer from "./components/HomepageContainer";
 import MenuBar from "./components/MenuBar";
+import useSearch from "./hooks/UseSearch";
 
 export default function Home() {
+  const { searchText, setSearchText } = useSearch();
+
   return (
     <main className="bg-[#ffffff]">
       <MenuBar head={"For you"} rank={true} search={true} />
-      <HomeContainer />
+      {searchText === "" ? <HomeContainer /> : <div>Searching</div>}
     </main>
   );
 }
