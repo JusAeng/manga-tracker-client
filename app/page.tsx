a431d174e3243451712a995ceedaf4003c8da62a
JusAeng <justo_play@hotmail.com>
2024-01-08 23:15:10 +0700

    try component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
-"use client";
-
 import HomeContainer from "./components/HomepageContainer";
 import MenuBar from "./components/MenuBar";
-import useSearch from "./hooks/UseSearch";
 
 export default function Home() {
-  const { searchText, setSearchText } = useSearch();
-
   return (
     <main className="bg-[#ffffff]">
       <MenuBar head={"For you"} rank={true} search={true} />
-      {searchText === "" ? <HomeContainer /> : <div>Searching</div>}
+      <HomeContainer />
     </main>
   );
 }
