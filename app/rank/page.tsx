100daf6dda89ad5fffe0ebc1d0e337edc437f36a
JusAeng <justo_play@hotmail.com>
2024-01-08 23:15:39 +0700

    create rank page

---
+++ app/rank/page.tsx
@@ -0,0 +1,14 @@
+import BackButton from "../components/BackButton";
+
+const RankPage = () => {
+  return (
+    <div>
+      <div className="h-[40px] bg-[#5c7df7] flex items-center gap-[20px]">
+        <BackButton />
+        <h2>Ranking</h2>
+      </div>
+    </div>
+  );
+};
+
+export default RankPage;
