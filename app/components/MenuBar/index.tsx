796ab3c445bdc086871ec3c281004d2bc3e844ff
JusAeng <justo_play@hotmail.com>
2024-01-08 23:14:12 +0700

    add rank menu

--- app/components/MenuBar/index.tsx
+++ app/components/MenuBar/index.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import useSearch from "@/app/hooks/UseSearch";
+import { useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
+import { RiSearchFill } from "react-icons/ri";
 
 interface Iprop {
   head: string;
@@ -11,6 +13,7 @@ interface Iprop {
 }
 
 const MenuBar: React.FC<Iprop> = ({ head, rank, sort, search }) => {
+  const router = useRouter();
   const [isSearch, setIsSearch] = useState(false);
   const [isFilter, setIsFilter] = useState(false);
 
@@ -30,6 +33,10 @@ const MenuBar: React.FC<Iprop> = ({ head, rank, sort, search }) => {
   const handleTypingSearch = () => {
     const textSearchRef = searchRef.current?.value || "";
     setSearchText(textSearchRef);
+  };
+
+  const handleRank = () => {
+    router.push("/rank");
   };
 
   useEffect(() => {
@@ -55,18 +62,14 @@ const MenuBar: React.FC<Iprop> = ({ head, rank, sort, search }) => {
       ) : (
         <div className="flex justify-between items-center px-[10px] w-[100%]">
           <h1 className="bg-[#ffffff]">{head}</h1>
-          <div className="flex justify-around bg-[#ffffff] gap-[20px]">
-            {rank && <div>rank</div>}
+          <div className="flex justify-around gap-[20px]">
+            {rank && <div onClick={handleRank}>rank</div>}
             {sort && (
               <div onClick={handleFilter}>
                 <div>sor</div>
               </div>
             )}
-            {search && (
-              <div onClick={handleSearch}>
-                <div>search</div>
-              </div>
-            )}
+            {search && <RiSearchFill size={20} onClick={handleSearch} />}
           </div>
         </div>
       )}
