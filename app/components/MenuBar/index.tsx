309e0bb6cbe11fcf5658614910df2aad58877748
JusAeng <justo_play@hotmail.com>
2024-01-09 23:34:25 +0700

    refactor & add sorting

--- app/components/MenuBar/index.tsx
+++ app/components/MenuBar/index.tsx
@@ -4,6 +4,7 @@ import useSearch from "@/app/hooks/UseSearch";
 import { useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 import { RiSearchFill } from "react-icons/ri";
+import { LuSettings2 } from "react-icons/lu";
 
 interface Iprop {
   head: string;
@@ -15,17 +16,17 @@ interface Iprop {
 const MenuBar: React.FC<Iprop> = ({ head, rank, sort, search }) => {
   const router = useRouter();
   const [isSearch, setIsSearch] = useState(false);
-  const [isFilter, setIsFilter] = useState(false);
+  const [isSort, setIsSort] = useState(false);
 
   const { searchText, setSearchText } = useSearch();
   const searchRef = useRef<HTMLInputElement>(null);
 
-  const handleFilter = () => {
-    setIsFilter(!isFilter);
+  const handleSort = () => {
+    setIsSort(!isSort);
   };
 
   const handleSearch = () => {
-    setIsFilter(false);
+    setIsSort(false);
     setSearchText("");
     setIsSearch(!isSearch);
   };
@@ -48,32 +49,44 @@ const MenuBar: React.FC<Iprop> = ({ head, rank, sort, search }) => {
   }, [isSearch]);
 
   return (
-    <main className="bg-[#5c7df7] h-[40px] grid place-items-center w-[100%] sticky top-[0px] z-20">
-      {isSearch ? (
-        <div className="flex justify-between px-[10px] w-[100%]">
-          <div onClick={handleSearch}>back</div>
-          <input
-            type="text"
-            onChange={handleTypingSearch}
-            ref={searchRef}
-            value={searchText}
-          />
-        </div>
-      ) : (
-        <div className="flex justify-between items-center px-[10px] w-[100%]">
-          <h1 className="bg-[#ffffff]">{head}</h1>
-          <div className="flex justify-around gap-[20px]">
-            {rank && <div onClick={handleRank}>rank</div>}
-            {sort && (
-              <div onClick={handleFilter}>
-                <div>sor</div>
-              </div>
-            )}
-            {search && <RiSearchFill size={20} onClick={handleSearch} />}
+    <div>
+      <main className="bg-[#5c7df7] h-[40px] grid place-items-center w-[100%] sticky top-[0px] z-20">
+        {isSearch ? (
+          <div className="flex justify-between px-[10px] w-[100%]">
+            <div onClick={handleSearch}>back</div>
+            <div className="flex gap-[10px] items-center">
+              <LuSettings2 size={20} />
+              <input
+                type="text"
+                onChange={handleTypingSearch}
+                ref={searchRef}
+                value={searchText}
+                className="w-[260px]"
+              />
+            </div>
           </div>
+        ) : (
+          <div className="flex justify-between items-center px-[10px] w-[100%]">
+            <h1 className="bg-[#ffffff]">{head}</h1>
+            <div className="flex justify-around gap-[20px]">
+              {rank && <div onClick={handleRank}>rank</div>}
+              {sort && (
+                <div onClick={handleSort}>
+                  <div>sor</div>
+                </div>
+              )}
+              {search && <RiSearchFill size={20} onClick={handleSearch} />}
+            </div>
+          </div>
+        )}
+      </main>
+      {isSort && (
+        <div className="flex flex-col items-center">
+          <div>subscribe date up</div>
+          <div>subscribe date down</div>
         </div>
       )}
-    </main>
+    </div>
   );
 };
 
