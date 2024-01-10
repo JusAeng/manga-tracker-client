c7997a0a244abcf8ce596e34fc78dd347c4e43e0
JusAeng <justo_play@hotmail.com>
2024-01-11 00:16:39 +0700

    create SubButton for animation

---
+++ app/components/SubButton/index.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React, { useState } from "react";
+import "./index.css"; // Import your CSS file for styling
+import { IoMdAdd, IoMdCheckmark } from "react-icons/io";
+
+const SubButton: React.FC = () => {
+  const [spin, setSpin] = useState(false);
+  const [changed, setChanged] = useState(false);
+
+  const handleClick = () => {
+    setSpin(true);
+
+    setTimeout(() => {
+      setChanged(!changed);
+      setSpin(false);
+    }, 1000);
+  };
+
+  return (
+    <div>
+      {changed ? (
+        // Your changed logo or icon goes here
+        <button className={spin ? "spin-back" : ""} onClick={handleClick}>
+          <IoMdCheckmark size={21} color={"#ffffff"} />
+        </button>
+      ) : (
+        <button className={spin ? "spin-forward" : ""} onClick={handleClick}>
+          <IoMdAdd size={21} color={"#ffffff"} />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default SubButton;
