e126610aa56f6637abf456ae80fa1427bfb8c1ad
JusAeng <justo_play@hotmail.com>
2024-01-11 07:24:41 +0700

    change author to publisher

--- app/components/TrendingCard/index.tsx
+++ app/components/TrendingCard/index.tsx
@@ -6,17 +6,17 @@ import { useRouter } from "next/navigation";
 interface IProp {
   image: string;
   name: string;
-  author: string;
+  publisher: string;
 }
 
-const TrendingCard: React.FC<IProp> = ({ image, name, author }) => {
+const TrendingCard: React.FC<IProp> = ({ image, name, publisher }) => {
   const router = useRouter();
   const handleView = () => {
     let temp = name.replace(/\s+/g, "-");
     router.push(`/manga-detail/${temp}`);
   };
   return (
-    <main onClick={handleView}>
+    <main onClick={handleView} className="cursor-pointer w-[100px]">
       <div className="relative w-[100px] h-[120px]">
         <Image
           src={image}
@@ -29,8 +29,15 @@ const TrendingCard: React.FC<IProp> = ({ image, name, author }) => {
           className="rounded-xl"
         />
       </div>
-      <p className="text-white">{name}</p>
-      <p className="text-white">{author}</p>
+      <div className="flex items-center gap-[5px] mt-[2px]">
+        <div className="w-[3px] bg-[#444444] h-[32px] rounded-[1px]"></div>
+        <div>
+          <p className="text-white truncate w-[100%] pr-[10px] text-[14px]">
+            {"One piece"}
+          </p>
+          <p className="text-[#777777] text-[11px]">{publisher}</p>
+        </div>
+      </div>
     </main>
   );
 };
