fcaa1285be9efcca054a0004473ad480e8fa3638
JusAeng <justo_play@hotmail.com>
2024-01-11 18:48:19 +0700

    recognize manga type

---
+++ app/temp/manga.d.ts
@@ -0,0 +1,16 @@
+interface MangaType {
+  id: string;
+  name: string;
+  otherNames: string[];
+  image: string;
+  author: string;
+  publisher: string;
+  introduction: string;
+  genre: string;
+  otherGenres: string[];
+  vols: number[];
+  volImages: string[];
+  subscribers: number;
+  score: number;
+  voter: number;
+}
