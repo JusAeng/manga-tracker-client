67e30f8d8d5da0c71cab9542c62d5f3ab897b913
JusAeng <justo_play@hotmail.com>
2024-01-11 23:05:06 +0700

    move type to new file

--- app/types/Anime.d.ts
+++ app/types/Anime.d.ts
@@ -4,3 +4,20 @@ export type AnimeType = {
   image: string;
   lastEpisode: number;
 };
+
+export type MangaType = {
+  id: string;
+  name: string;
+  otherNames: string[];
+  image: string;
+  author: string;
+  publisher: string;
+  introduction: string;
+  genre: string;
+  otherGenres: string[];
+  vols: number[];
+  volImages: string[];
+  subscribers: number;
+  score: number;
+  voter: number;
+};
