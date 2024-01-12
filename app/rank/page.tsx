47c1510846e8a6b4a3f57dd6d35fda37fdfc66f5
JusAeng <justo_play@hotmail.com>
2024-01-12 21:55:12 +0700

    rank page

--- app/rank/page.tsx
+++ app/rank/page.tsx
@@ -1,13 +1,40 @@
 import BackButton from "../components/BackButton";
+import mangas from "@/app/temp/anime.json";
+import RankCard from "./RankCard";
 
 const RankPage = () => {
   return (
-    <div>
-      <div className="h-[40px] bg-[#5c7df7] flex items-center gap-[20px]">
-        <BackButton size={20} color="000000" />
-        <h2>Ranking</h2>
+    <main className="min-h-screen bf-[#1e1e1f]">
+      <div className="h-[45px] bg-[#2e2e2f] flex items-center gap-[20px] px-[10px]">
+        <BackButton size={22} color={"#ffffff"} classAdd=" " />
+        <h1 className="text-[#ffffff]">Ranking</h1>
       </div>
-    </div>
+      <div>
+        <section className="text-[#ffffff]">Monthly Hot</section>
+        <div className="flex justify-center gap-[1px]">
+          {/* <section className="text-[#ffffff] bg-[#333333] w-[190px]">
+            <h3>Top Score</h3>
+          </section> */}
+          <section className="text-[#ffffff] bg-[#333333] w-[190px]">
+            <h3>Top Subscribe</h3>
+            <div className="flex flex-col gap-[10px] items-center">
+              {mangas.map((m) => {
+                return (
+                  <RankCard
+                    key={m.name}
+                    image={m.image}
+                    name={m.name}
+                    subscriber={7}
+                    score={5}
+                    rankType="subscribe"
+                  />
+                );
+              })}
+            </div>
+          </section>
+        </div>
+      </div>
+    </main>
   );
 };
 
