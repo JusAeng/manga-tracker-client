45e98629af13449597560c2d66249178eec7182b
JusAeng <justo_play@hotmail.com>
2024-02-29 15:26:38 +0700

    mod: new ui manga-card-component

--- app/components/MangaCard/index.tsx
+++ app/components/MangaCard/index.tsx
@@ -1,17 +1,22 @@
 "use client";
 
-import Image from "next/image";
+import ImageUtil from "../ImageUtil";
 import { useRouter } from "next/navigation";
-import "./truncate.css";
+import { StaticImageData } from "next/image";
 
-interface Iprop {
-  image: string;
+interface IMangaCard {
+  image: string | StaticImageData;
   name: string;
   author: string;
-  lastEpisode: number;
+  lastVol: number;
 }
 
-const MangaCard: React.FC<Iprop> = ({ image, name, author, lastEpisode }) => {
+const MangaCardNew: React.FC<IMangaCard> = ({
+  image,
+  name,
+  author,
+  lastVol,
+}) => {
   const router = useRouter();
   const handleClick = () => {
     let temp = name.replace(/\s+/g, "-");
@@ -19,41 +24,20 @@ const MangaCard: React.FC<Iprop> = ({ image, name, author, lastEpisode }) => {
   };
 
   return (
-    <main
-      className="flex bg-[#3f3f3f] w-[340px] h-[96px] rounded-xl cursor-pointer"
-      onClick={() => handleClick()}
-    >
-      <section className="m-[10px] grid place-items-center w-[25%]">
-        <div className="relative w-[74px] h-[74px]">
-          <Image
-            src={image}
-            alt="Picture of the author"
-            fill
-            sizes="100%"
-            style={{
-              objectFit: "cover",
-            }}
-            className="rounded-xl"
-          />
-        </div>
-      </section>
-      <section className="m-[10px] flex flex-col justify-evenly w-[60%] items-start">
-        <div className="truncate-container">
-          <h3
-            className={
-              "text-[#ffffff]" +
-              (name.length > 32 ? " text-[15px]" : " text-[17px]")
-            }
-          >
-            {name}
-          </h3>
-        </div>
-        <span className="bg-[#68de7c] rounded-lg py-[1px] px-[6px] text-[11px] text-[#333333]">
-          Latest vol: {lastEpisode}
-        </span>
-      </section>
-    </main>
+    <div className="w-[150px]" onClick={handleClick}>
+      <ImageUtil
+        image={image}
+        w={150}
+        h={222}
+        objectFit="cover"
+        imageClass="rounded-[10px] cursor-pointer"
+      />
+      <h3 className="text-white text-center px-[3px] truncate">{name}</h3>
+      <p className="relative bottom-[3px] text-white text-center text-[12px]">
+        lastest vol: {lastVol}
+      </p>
+    </div>
   );
 };
 
-export default MangaCard;
+export default MangaCardNew;
