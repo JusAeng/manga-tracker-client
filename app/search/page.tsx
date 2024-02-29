51504df73d42fc26215f09a35756463860f22c9c
JusAeng <justo_play@hotmail.com>
2024-03-01 04:02:13 +0700

    create: search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,8 +1,22 @@
+import testData from "@/app/temp/anime.json";
+import TrendingCard from "../components/TrendingCard";
+
 const SearchPage = () => {
+  const myAnimes = testData;
   return (
     <main className="main-page">
-      <section>n</section>
-      searchPage
+      <div className="flex flex-wrap gap-5">
+        {myAnimes.map((anime) => (
+          <TrendingCard
+            key={anime.name}
+            image={anime.image}
+            name={anime.name}
+            publisher={anime.publisher}
+            w={80}
+            h={120}
+          />
+        ))}
+      </div>
     </main>
   );
 };
