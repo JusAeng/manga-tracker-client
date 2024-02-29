e64f6b113b8be2373ccc27bf2c00fed10c05656a
JusAeng <justo_play@hotmail.com>
2024-02-29 16:53:10 +0700

    fix: trunecate

--- app/components/TrendingCard/index.tsx
+++ app/components/TrendingCard/index.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import ImageUtil from "../ImageUtil";
 
 interface IProp {
   image: string;
@@ -17,7 +18,7 @@ const TrendingCard: React.FC<IProp> = ({ image, name, publisher }) => {
   };
   return (
     <main onClick={handleView} className="cursor-pointer w-[100px]">
-      <div className="relative w-[100px] h-[120px]">
+      {/* <div className="relative w-[100px] h-[120px]">
         <Image
           src={image}
           alt="Picture of the author"
@@ -28,14 +29,13 @@ const TrendingCard: React.FC<IProp> = ({ image, name, publisher }) => {
           }}
           className="rounded-xl"
         />
-      </div>
+      </div> */}
+      <ImageUtil image={image} w={100} h={150} imageClass="rounded-xl" />
       <div className="flex items-center gap-[5px] mt-[2px]">
         <div className="w-[3px] bg-[#444444] h-[32px] rounded-[1px]"></div>
-        <div>
-          <p className="text-white truncate w-[100%] pr-[10px] text-[14px]">
-            {"One piece"}
-          </p>
-          <p className="text-[#777777] text-[11px]">{publisher}</p>
+        <div className="w-[90px]">
+          <p className="text-white text-[14px] truncate">{name}</p>
+          <p className="text-[#777777] text-[11px] truncate">{publisher}</p>
         </div>
       </div>
     </main>
