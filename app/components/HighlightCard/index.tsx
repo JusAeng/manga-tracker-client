6c81a8b579dded7e4183794988c4d9d5e0c24105
JusAeng <justo_play@hotmail.com>
2024-02-29 15:45:22 +0700

    mod: scale 2:3

--- app/components/HighlightCard/index.tsx
+++ app/components/HighlightCard/index.tsx
@@ -33,7 +33,7 @@ const HighlightCard: React.FC<IProp> = ({ image, genres, name }) => {
         }}
         onClick={handleView}
       ></div> */}
-      <div className="relative w-[360px] h-[445px] cursor-pointer">
+      <div className="relative w-[340px] h-[510px] cursor-pointer">
         <Image
           src={image}
           alt="Picture of the author"
