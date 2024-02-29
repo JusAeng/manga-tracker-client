70600926009334ccca5047d5147d2962ef36ee43
JusAeng <justo_play@hotmail.com>
2024-03-01 06:33:45 +0700

    refactor: code

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,22 +1,9 @@
-import testData from "@/app/temp/anime.json";
-import TrendingCard from "../components/TrendingCard";
+import SearchContainer from "./SearchContainer";
 
 const SearchPage = () => {
-  const myAnimes = testData;
   return (
     <main className="main-page">
-      <div className="flex flex-wrap gap-5">
-        {myAnimes.map((anime) => (
-          <TrendingCard
-            key={anime.name}
-            image={anime.image}
-            name={anime.name}
-            publisher={anime.publisher}
-            w={80}
-            h={120}
-          />
-        ))}
-      </div>
+      <SearchContainer />
     </main>
   );
 };
