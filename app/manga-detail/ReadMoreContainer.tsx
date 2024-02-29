55493ed85ab4320796cbba9f0a88e55cca4034b2
JusAeng <justo_play@hotmail.com>
2024-02-29 13:21:01 +0700

    mod: rounded & relate up to cover pic

--- app/manga-detail/ReadMoreContainer.tsx
+++ app/manga-detail/ReadMoreContainer.tsx
@@ -9,11 +9,11 @@ const GradientText: React.FC = () => {
   const [fullShow, setFullShow] = useState(false);
 
   return (
-    <main className="flex flex-col bg-[#1e1e1f] w-[100vw]">
-      <div className="mx-[8px] mt-[3px]">
+    <main className="flex flex-col bg-[#1e1e1f] w-[100vw] rounded-t-[20px] px-[12px]">
+      <div className="mt-[5px]">
         <div className="flex items-center justify-between">
           <h3 className="text-[22px] text-[#ffffff]">Rent Girlfriend</h3>
-          <div className="flex mr-[10px] items-center gap-[6px] relative">
+          <div className="flex items-center gap-[6px] relative">
             <FaStar size={18} color={"#f7bc63"} />
             <h3 className="text-[18px] text-[#ffffff] mt-[3px]">5</h3>
           </div>
@@ -26,12 +26,11 @@ const GradientText: React.FC = () => {
       </div>
       <div
         className={
-          "px-[8px]" +
-          (!fullShow ? " readmore-container-less" : " readmore-container-full")
+          !fullShow ? " readmore-container-less" : " readmore-container-full"
         }
       >
         <p className="leading-4 mt-[2px] text-[#ffffff]">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam
+          Lorem, ipsum dolor sit amet consectetur adipis icing elit. Ullam
           deserunt voluptate voluptates ab neque porro aliquid totam recusandae
           ratione repellendus. Consequatur reprehenderit error quae sapiente,
           soluta cum repellendus quis aperiam. Lorem ipsum dolor sit amet
--- app/manga-detail/[mangaName]/page.tsx
+++ app/manga-detail/[mangaName]/page.tsx
@@ -17,8 +17,8 @@ export default function AnimeDetailPage({
   }, [setNavText]);
   return (
     <main className="bg-[#1e1e1f] min-h-screen">
-      <div
-        className="h-[30vh] bg-[#5c7df7] flex flex-col items-start justify-between"
+      <section
+        className="h-[32vh] bg-[#5c7df7] flex flex-col items-start justify-between pb-[15px]"
         style={{
           background:
             'url("https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/09/rent-a-girlfriend-episode-10-season-2-ruka-sarashina-kiss-whenever.jpg")',
@@ -30,13 +30,15 @@ export default function AnimeDetailPage({
         <div className="bg-[#ffffff] rounded-[5px] text-[12px] mb-[10px] ml-[5px] px-[4px]">
           Comady
         </div>
-      </div>
-      <ReadMoreContainer />
-      <div className="container flex flex-col gap-[5px] mt-[10px] pb-[50px]">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((v) => (
-          <VolumnCard key={v} mangaName="Hunter" vol={v} />
-        ))}
-      </div>
+      </section>
+      <section className="relative bottom-[15px]">
+        <ReadMoreContainer />
+        <div className="container flex flex-col gap-[5px] mt-[10px] pb-[50px]">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((v) => (
+            <VolumnCard key={v} mangaName="Hunter" vol={v} />
+          ))}
+        </div>
+      </section>
     </main>
   );
 }
