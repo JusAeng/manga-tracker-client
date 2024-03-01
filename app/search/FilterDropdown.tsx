5b2244da2fe6c2784f62fc0f37c12b4a655d3fe1
JusAeng <justo_play@hotmail.com>
2024-03-02 00:24:38 +0700

    style

--- app/search/FilterDropdown.tsx
+++ app/search/FilterDropdown.tsx
@@ -10,14 +10,52 @@ interface DropdownMenuType {
 
 const FilterDropdown: React.FC<IProp> = ({ option, added, callback }) => {
   const dropdownMenu: DropdownMenuType = {
-    genres: ["Romatic comedy"],
+    genres: [
+      "Action",
+      "Adventure",
+      "Comedy",
+      "Drama",
+      "Fantasy",
+      "Horror",
+      "Mystery",
+      "Romance",
+      "Science Fiction (Sci-Fi)",
+      "Slice of Life",
+      "Supernatural",
+      "Thriller",
+      "Mecha",
+      "Psychological",
+      "Sports",
+      "Shounen",
+      "Shoujo",
+      "Seinen",
+      "Josei",
+      "Harem",
+      "Ecchi",
+      "Historical",
+      "Music",
+      "Magic",
+      "School",
+      "Space",
+      "Super Power",
+      "Martial Arts",
+      "Military",
+      "Post-Apocalyptic",
+    ],
     publishers: ["Siam Inter Comic", "Luckpim"],
-    sort: [""],
+    sort: ["A -> Z"],
   };
   return (
-    <div className="flex flex-col bg-[#aaaaaa] absolute top-[100%] left-0 h-[40vh] w-[100vw] mt-[5px]">
+    <div
+      className="flex flex-wrap gap-x-[18px] px-[5px] pt-[5px] pb-[16px] gap-y-[10px] bg-primaryx 
+    absolute top-[100%] left-0 w-[100vw] min-h-[10vh] max-h-[30vh] overflow-auto mt-[5px] rounded-b-[8px]"
+    >
       {dropdownMenu[option].map((optionMenu) => (
-        <button key={optionMenu} onClick={() => callback(optionMenu)}>
+        <button
+          key={optionMenu}
+          onClick={() => callback(optionMenu)}
+          className="py-[2px] px-[10px] h-fit rounded-[15px] bg-[#232323] text-[#bbbbbb]"
+        >
           {optionMenu}
         </button>
       ))}
