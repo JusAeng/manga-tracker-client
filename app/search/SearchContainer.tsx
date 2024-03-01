5d47914351b7d2f5a76c4cf70d99436be0fd58f9
JusAeng <justo_play@hotmail.com>
2024-03-01 23:59:23 +0700

    feat: dropdown filter highligh

--- app/search/SearchContainer.tsx
+++ app/search/SearchContainer.tsx
@@ -2,45 +2,50 @@
 
 import testData from "@/app/temp/anime.json";
 import { MdFilterAlt } from "react-icons/md";
-import { IoChevronBackOutline } from "react-icons/io5";
+import { IoChevronBackOutline, IoChevronDownOutline } from "react-icons/io5";
 import MangaCardMini from "./MangaCardMini";
 import { motion } from "framer-motion";
 import { useRef, useState } from "react";
+import FilterDropdown from "./FilterDropdown";
 
 interface FilterElementsType {
   accessKey: string;
   header: string;
-  dropdown: JSX.Element;
+}
+
+interface AddedType {
+  [key: string]: string[];
 }
 
 const allFilterElements: FilterElementsType[] = [
   {
-    accessKey: "Publisher",
-    header: "publisher",
-    dropdown: <></>,
+    accessKey: "publishers",
+    header: "Publishers",
   },
   {
-    accessKey: "Genres",
-    header: "genres",
-    dropdown: <></>,
+    accessKey: "genres",
+    header: "Genres",
   },
   {
-    accessKey: "Sort",
-    header: "sort",
-    dropdown: <></>,
+    accessKey: "sort",
+    header: "Sort",
   },
 ];
 
 const SearchContainer = () => {
-  const myAnimes = testData;
+  const myManga = testData;
 
   const searchRef = useRef<HTMLInputElement>(null);
   const [isSearching, setIsSearching] = useState(false);
   const [searchText, setSearchText] = useState("");
 
   const [isFilter, setIsFilter] = useState(false);
-  const [filter, setFilter] = useState([] as string[]);
   const [filterOption, setFilterOption] = useState("");
+  const [filterItems, setFilterItems] = useState({
+    publishers: [],
+    genres: [],
+    sort: [],
+  } as AddedType);
 
   const handleTyping = () => {
     const textSearchRef = searchRef.current?.value || "";
@@ -54,19 +59,25 @@ const SearchContainer = () => {
       setFilterOption(option);
     }
   };
+  const toggleFilter = () => {
+    setIsFilter(!isFilter);
+    setFilterOption("");
+  };
+
   const handleInputFocus = () => {
     setIsSearching(true);
     setIsFilter(false);
+    setFilterOption("");
   };
 
-  let filteredMyAnime = myAnimes?.filter((manga) =>
+  let filteredMyManga = myManga?.filter((manga) =>
     manga.name.toLowerCase().includes(searchText.toLowerCase())
   );
 
   return (
     <main className="pb-[10px]">
       <section className="sticky top-[0px] z-20 bg-primaryx">
-        <section className="pt-[20px] pb-[10px] px-[10px] flex justify-evenly items-center relative">
+        <section className="pt-[20px] pb-[10px] px-[10px] flex justify-around items-center relative">
           {isSearching && (
             <div
               onClick={() => setIsSearching(false)}
@@ -94,44 +105,44 @@ const SearchContainer = () => {
             ref={searchRef}
             onChange={handleTyping}
           />
-          <div
-            className="p-[5px] cursor-pointer"
-            onClick={() => setIsFilter(!isFilter)}
-          >
+          <div className="p-[5px] cursor-pointer" onClick={toggleFilter}>
             <MdFilterAlt size={24} color={"#ffffff"} />
           </div>
         </section>
         {isFilter ? (
           <motion.section
-            className="bg-primaryx absolute w-[100%] z-20 text-white flex justify-around pb-[10px]"
+            className="bg-primaryx absolute w-[100%] z-20 pb-[16px] rounded-b-[8px]"
             animate={{ y: 0, opacity: 1 }}
             initial={{ y: "-30%", opacity: 0 }}
             transition={{ duration: 0.2 }}
           >
-            {allFilterElements.map((element) => (
-              <div key={element.accessKey} className="flex flex-col">
-                <button onClick={() => handleFilterOption(element.accessKey)}>
-                  {element.header}
-                </button>
-                {filterOption === element.accessKey ? element.dropdown : <></>}
-              </div>
-            ))}
-            {/* <div className="flex flex-col">
-              <button onClick={() => handleFilterOption("publisher")}>
-                Publisher
-              </button>
-              {filterOption === "publisher" ? <>nani</> : <></>}
+            <div className="text-white grid grid-cols-3 relative">
+              {allFilterElements.map((element) => (
+                <div
+                  key={element.accessKey}
+                  className="flex flex-col border border-[#555555] items-center"
+                >
+                  <button
+                    onClick={() => handleFilterOption(element.accessKey)}
+                    className={`flex items-center gap-[4px] w-fit py-[2px] px-[10px] rounded-[15px] ${
+                      element.accessKey === filterOption ? "bg-[#ffaaaa]" : ""
+                    }`}
+                  >
+                    <p>{element.header}</p>
+                    <IoChevronDownOutline size={12} />
+                  </button>
+                  {filterOption === element.accessKey ? (
+                    <FilterDropdown
+                      option={element.accessKey}
+                      added={[]}
+                      callback={(optionMenu: string) => console.log(optionMenu)}
+                    />
+                  ) : (
+                    <></>
+                  )}
+                </div>
+              ))}
             </div>
-            <div className="flex flex-col">
-              <button onClick={() => handleFilterOption("genres")}>
-                Genres
-              </button>
-              {filterOption === "genres" ? <>nani</> : <></>}
-            </div>
-            <div className="flex flex-col">
-              <button onClick={() => handleFilterOption("sort")}>Sort</button>
-              {filterOption === "sort" ? <>nani</> : <></>}
-            </div> */}
           </motion.section>
         ) : (
           <></>
@@ -139,7 +150,7 @@ const SearchContainer = () => {
       </section>
 
       <section className="grid grid-cols-3 place-items-center gap-y-[10px] px-[4px] pt-[10px]">
-        {filteredMyAnime.map((anime) => (
+        {filteredMyManga.map((anime) => (
           <MangaCardMini
             key={anime.name}
             id={""}
@@ -147,7 +158,7 @@ const SearchContainer = () => {
             name={anime.name}
           />
         ))}
-        {myAnimes.map((anime) => (
+        {myManga.map((anime) => (
           <MangaCardMini
             key={anime.name}
             id={""}
