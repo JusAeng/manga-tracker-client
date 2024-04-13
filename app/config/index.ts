431d00b223c46c8e4b7cffbbb35daf1126817c9c
JusAeng <justo_play@hotmail.com>
2024-04-13 22:14:59 +0700

    config env

---
+++ app/config/index.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+interface ProcessEnv {
+  LINE_CHANNEL_ACCESS_TOKEN?: string;
+  LIFF_ID?: string;
+  BASE_URL?: string;
+}
+
+const config: ProcessEnv = {
+  LINE_CHANNEL_ACCESS_TOKEN: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  LIFF_ID: process.env.LIFF_ID,
+  BASE_URL: process.env.BASE_URL,
+};
+
+export default config;
