94c722647d70e3c041540dbfa17d042bf892ac0c
JusAeng <justo_play@hotmail.com>
2024-04-18 20:38:40 +0700

    fetch realdata

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,9 +1,13 @@
 import SearchContainer from "./SearchContainer";
+import axiosInstance from "../utils/axios";
 
-const SearchPage = () => {
+const SearchPage = async () => {
+  const res = await axiosInstance.get("/manga");
+  const allManga = res.data || [];
+
   return (
     <main className="main-page scrollbar-hide">
-      <SearchContainer />
+      <SearchContainer allManga={allManga} />
     </main>
   );
 };
