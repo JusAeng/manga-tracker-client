b533ead35c759accd16a4a60d3bdd61224831a4f
JusAeng <justo_play@hotmail.com>
2024-04-20 00:53:53 +0700

    handle no image

--- app/manga-detail/VolumnCard.tsx
+++ app/manga-detail/VolumnCard.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { IoBook } from "react-icons/io5";
 import axiosInstance from "../utils/axios";
 import UseProfile from "../hooks/UseProfile";
+import ImageUtil from "../components/ImageUtil";
 
 interface Iprop {
   mangaId: string;
@@ -34,20 +35,9 @@ const VolumnCard: React.FC<Iprop> = ({ mangaId, image, vol, own }) => {
   };
 
   return (
-    <div className="flex justify-between h-[72px] items-center px-[20px] border-b-[1px] border-[#303030] pb-[10px]">
+    <div className="flex justify-between h-[72px] items-center px-[20px] border-b-[1px] border-[#303030]">
       <section className="flex items-center gap-[20px]">
-        <div className="relative w-[40px] h-[60px]">
-          <Image
-            src={image}
-            alt="Picture of the author"
-            fill
-            sizes="100%"
-            style={{
-              objectFit: "cover",
-            }}
-            className="rounded-[4px]"
-          />
-        </div>
+        <ImageUtil image={image} w={40} h={60} />
         {/* <div className="relative w-[40px] h-[60px] bg-[#333333] rounded-[4px] grid place-items-center text-[#4c4c4c]">
           {vol > 9 ? vol : "0" + vol}
         </div> */}
