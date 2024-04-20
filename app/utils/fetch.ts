05899e4c4ba420394ce41c7a3684ac9e0984038e
JusAeng <justo_play@hotmail.com>
2024-04-21 05:29:52 +0700

    temp config

--- app/utils/fetch.ts
+++ app/utils/fetch.ts
@@ -1,12 +1,15 @@
+import configEnv from "../config";
+
 export async function fetchUtil(
   endpoint: string,
   options: RequestInit = {}
 ): Promise<any> {
-  const baseUrl = "";
+  const baseUrl = configEnv.BASE_URL || "";
 
   const url = baseUrl + endpoint;
 
-  const token = "";
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjIwMTg2MDAsInJvbGUiOiJ1c2VyIiwidXNlcklkIjoiOGE5OTRiNzYyYzM4OGRhOTM0YWI0NzM2In0.FZHRYu06uBkpgn_pvkOmGLcFKVgGpxSlSdY1pBuTubo";
 
   const headers = {
     ...options.headers,
