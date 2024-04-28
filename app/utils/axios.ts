acb4faa4910ccf4fcf902d218213001de34e3e9f
JusAeng <justo_play@hotmail.com>
2024-04-29 05:16:44 +0700

    axios

--- app/utils/axios.ts
+++ app/utils/axios.ts
@@ -2,10 +2,6 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://mgt-backend-deploy-spk23ljzqq-as.a.run.app",
-  headers: {
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjIwMTg2MDAsInJvbGUiOiJ1c2VyIiwidXNlcklkIjoiOGE5OTRiNzYyYzM4OGRhOTM0YWI0NzM2In0.FZHRYu06uBkpgn_pvkOmGLcFKVgGpxSlSdY1pBuTubo",
-  },
 });
 
 export default axiosInstance;
