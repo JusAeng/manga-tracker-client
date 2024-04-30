ad82c8fb33d8d85381b56dcb52d86a69bbfce9be
JusAeng <justo_play@hotmail.com>
2024-04-30 07:26:33 +0700

    dont use

--- app/api/subscribelist/route.ts
+++ app/api/subscribelist/route.ts
@@ -1,18 +1,1 @@
-import { fetchUtil } from "@/app/utils/fetch";
 
-export async function GET() {
-  try {
-    const res = await fetchUtil("/user/subscribelist", {
-      cache: "no-store",
-    });
-    return Response.json({
-      data: res,
-    });
-  } catch (e) {
-    console.log(e);
-  }
-
-  return Response.json({
-    data: [],
-  });
-}
